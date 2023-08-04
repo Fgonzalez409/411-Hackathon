cf1b2801e856c6ffced72454ec18e7e5553133f0
Fgonzalez409 <fergonz409@gmail.com>
2023-08-03 20:27:45 -0500

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import "normalize.css"
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-<<<<<<< HEAD
-    <body>
-    <div className="header-container">
-      <header className="sticky-header">
-        <div className="logo">Your Logo</div>
-        <div className="search-bar-container">
-          <input type="text" placeholder="Search by title, url or author" />
-          <button type="submit">Search</button>
-        </div>
-      </header>
-    </div>
-
-    
-    </body>
-=======
-      
->>>>>>> 09067ea06d3ca2330620bea536b7f4ec14326683
-    </>
-  )
-}
-
-export default App
-=======
-
 import React, { useState, useEffect } from 'react';
+import "normalize.css"
 import './App.css'
 
 function FetchDataComponent() {
@@ -53,7 +20,24 @@ function FetchDataComponent() {
   }, []);
 
   return (
+
+    
     <div>
+      <body>
+    <div className="header-container">
+      <header className="sticky-header">
+        <div className="logo">Your Logo</div>
+        <div className="search-bar-container">
+          <input type="text" placeholder="Search by title, url or author" />
+          <button type="submit">Search</button>
+        </div>
+      </header>
+    </div>
+
+    
+    </body>
+
+
       {data ? (
         <div>
           {data.hits.map(item => (
@@ -80,4 +64,3 @@ function App() {
 }
 
 export default App;
->>>>>>> c4e1f8d75e51d4de53d5b58e7319ce49fb8db96f
