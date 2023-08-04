1faab7ed5f771721785b4609511e9d2ea86ce04f
PeytenBevill <redsage00@gmail.com>
2023-08-03 20:51:24 -0500

    complete merge request

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import './App.css';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "normalize.css"
+import './App.css'
 
 function App() {
   const [data, setData] = useState(null);
@@ -24,7 +27,8 @@ function App() {
   }, [data]);
 
   return (
-    <div>
+    <>
+      <div>
       <SearchBar items={data} />
       {data ? (
         <div>
@@ -41,8 +45,27 @@ function App() {
       ) : (
         <p>Loading data...</p>
       )}
-    </div>
-  );
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App;
